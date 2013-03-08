b919c2ee6e3d2af95d9eee58e660987c2454feaf
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 03:42:14 +0200

    mango: some cleanup
    * Removed Phone overlay (mdpi fix is merged in Phone app)
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ie2321f467202796cef44aa789e8c010c60d084a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,21 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/mango/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_mango_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/mango/recovery/init.rc
 
-#Sensors
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA250_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
 TARGET_OTA_ASSERT_DEVICE := SK17i,SK17a,mango
 
-#Bluetooth
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/mango/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/semc/mango/bluetooth/vnd_mango.txt
