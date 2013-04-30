561f9e5411ffa14dd1d08066829d3c5ccb44ae68
Michael Bestas <mikeioannina@gmail.com>
2013-04-30 04:36:05 +0300

    mango: remove bootrec-device
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ie255a217d15acc8fb80a65bc1740a71b67f92a6f

--- mango.mk
+++ mango.mk
@@ -20,10 +20,6 @@ PRODUCT_COPY_FILES += \
     device/semc/mango/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/initlogo.rle
 
-# Device specific part for two-stage boot
-PRODUCT_COPY_FILES += \
-    device/semc/mango/recovery/bootrec-device:recovery/bootrec-device
-
 # Device specific configs
 PRODUCT_COPY_FILES += \
     device/semc/mango/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
