3b71c67b0b37d4a2f852592c7f257b2ba7f88780
Michael Bestas <mikeioannina@gmail.com>
2013-06-10 21:28:24 +0300

    mango: little cleanup
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I599eb8e31f8f79a868002a6cd0b90718e2d8d029

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,13 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/mango/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/mango/bluedroid
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
 # Kernel
 TARGET_KERNEL_CONFIG := nAa_mango_defconfig
 
@@ -22,7 +29,5 @@ TARGET_KERNEL_CONFIG := nAa_mango_defconfig
 SOMC_CFG_SENSORS_ACCEL_BMA250_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := SK17i,SK17a,mango
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/mango/bluedroid
