afce8dfb6e0cca5f06275237cd2d10f69ae24f80
Michael Bestas <mikeioannina@gmail.com>
2013-06-11 13:47:19 +0300

    Fix build
    Change-Id: Ibe6f2c67820b4d0d9891b10d366d566233000863
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- full_mango.mk
+++ full_mango.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mango device
 $(call inherit-product, device/semc/mango/mango.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_mango
 PRODUCT_DEVICE := mango
