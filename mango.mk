66f0a36653ed696d5d4c932a81d336217fc594c4
Michael Bestas <mikeioannina@gmail.com>
2013-09-14 09:38:02 +0300

    mango: inherit hardware keyboard overlay
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I8dd2be2729fa1edf0206dc478c246e93d758f0f7

--- mango.mk
+++ mango.mk
@@ -16,6 +16,7 @@
 $(call inherit-product, vendor/semc/mango/mango-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/semc/mango/overlay
+DEVICE_PACKAGE_OVERLAYS += device/semc/mogami-common/overlay-keyboard
 
 $(call inherit-product, device/semc/mogami-common/mogami.mk)
 
