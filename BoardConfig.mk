399a589f9b655b2d6ce75d0e48e31042acf67b08
Michael Bestas <mikeioannina@gmail.com>
2013-10-16 00:29:51 +0300

    Fix for common CMHW
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,4 +31,4 @@ SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 TARGET_OTA_ASSERT_DEVICE := SK17i,SK17a,mango
 
 # CM Hardware tunables
-BOARD_HARDWARE_CLASS := device/semc/mango/cmhw
+BOARD_HARDWARE_CLASS += device/semc/mango/cmhw
