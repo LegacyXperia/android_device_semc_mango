62261249683e8b1cd0004e9059b2aac6d6a17cd9
Michael Bestas <mikeioannina@gmail.com>
2014-02-07 17:12:32 +0200

    mango: Fix device name
    Change-Id: Ibd2d4535ebaf7502675dd94fcf29285c34b9c4be

--- cm.mk
+++ cm.mk
@@ -33,9 +33,12 @@ PRODUCT_PACKAGES += \
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=SK17i \
+    PRODUCT_DEVICE=SK17i \
     BUILD_FINGERPRINT="SEMC/SK17i_1254-2184/SK17i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
     PRIVATE_BUILD_DESC="SK17i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_mango
-PRODUCT_DEVICE := mango
+
+# Release name
+PRODUCT_RELEASE_NAME := mango
