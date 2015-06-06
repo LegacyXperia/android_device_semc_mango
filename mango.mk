f3819095fb4a1f46f3e9828bdb6c2a9e4ee9f7a5
Michael Bestas <mikeioannina@gmail.com>
2015-06-06 16:49:30 +0300

    mango: Commonize init.semc.usb.rc
    Change-Id: I52b7748da86eef0a6c5068fa4bdfa2ab8d1d919e

--- mango.mk
+++ mango.mk
@@ -33,7 +33,6 @@ PRODUCT_AAPT_PREF_CONFIG := mdpi
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.device.rc:root/init.device.rc \
-    $(LOCAL_PATH)/rootdir/init.semc.usb.rc:root/init.semc.usb.rc \
     $(LOCAL_PATH)/rootdir/system/etc/hw_config.sh:system/etc/hw_config.sh
 
 # Device specific configs
@@ -52,4 +51,5 @@ PRODUCT_PACKAGES += \
 
 # Device properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160
+    ro.sf.lcd_density=160 \
+    ro.usb.pid_suffix=15A
